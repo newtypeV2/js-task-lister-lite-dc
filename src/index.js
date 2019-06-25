8c108c6e3577254260d6a928ea2b57e2c9500204
newtypeV2 <gianlibunao@gmail.com>
2019-06-25 19:57:06 -0400

    Added divCreator and divExists to accomodate priority option.

--- src/index.js
+++ src/index.js
@@ -7,40 +7,65 @@ let textField = document.getElementById('new-task-description');
 
 //tasks that are listed
 let tasks = document.getElementById('tasks');
+let priority = document.getElementById('priority');
 
+form.addEventListener('submit',submitHandler);
 
-document.addEventListener("DOMContentLoaded", () => {
-  //add a new event listener for the submission of a new list item
-  form.addEventListener('submit',submitHandler);
+function divExist(priority){
+  return document.getElementById(`div-${priority}`) ? true : false
+}
 
-    //handler for submit button for todo list item
-    function submitHandler(e) {
-
-    //create a new list item within a list tag
-    let newTodo = document.createElement('li');
-
-    //todo inner text is what is inputted within the text field
-    newTodo.innerText = textField.value;
-
-    //add the new todo list item to the list of tasks
-    tasks.appendChild(newTodo);
-
-    //create a new button element
-    let newButton = document.createElement('button')
-
-    //add the text inside the button
-    newButton.innerText = 'X';
-
-    //add the button to the new list item
-    newTodo.appendChild(newButton);
-
-    //add an event listener to delete the new list item
-    newButton.addEventListener('click', function() {
-      newTodo.remove(); //remove the newtodo item
+function createNewLi(){
+  let newTodo = document.createElement('li');
+  newTodo.innerText = textField.value;
+  let newButton = document.createElement('button')
+  newButton.innerText = 'X';
+  newTodo.appendChild(newButton);
+  newButton.addEventListener('click', function() {
+    newTodo.remove();
     });
+  return newTodo;
+}
 
-  //prevent from auto-refreshing
-  e.preventDefault();
-  };
-});
+function divCreator(priority){
+  if (priority==="high"){
+    let newDiv = document.createElement("div");
+    newDiv.id = "div-high"
+    newDiv.style.backgroundColor="red";
+    return newDiv;
+  }else if(priority==="medium"){ 
+    let newDiv = document.createElement("div");
+    newDiv.id = "div-high"
+    newDiv.style.backgroundColor="yellow";
+    return newDiv;
+  }else{
+    let newDiv = document.createElement("div");
+    newDiv.id = "div-high"
+    newDiv.style.backgroundColor="green";
+    return newDiv;
+  }
+}
+
+
+  //add a new event listener for the submission of a new list item
+  
+    //handler for submit button for todo list item
+function submitHandler(e) {
+  if (textField.value){
+    let newLi=createNewLi();
+    if (divExist(priority.value)){
+      let priorityDiv = document.getElementById(`div-${priority.value}`);
+      priorityDiv.appendChild(newLi);
+    }else{
+      let newDiv = divCreator(priority.value);
+      newDiv.appendChild(newLi);
+      tasks.appendChild(newDiv);
+    }
+    e.preventDefault();
+    form.reset();
+  }else{
+    alert("Textfield can't be empty.");
+    e.preventDefault();
+  }
+};
 
