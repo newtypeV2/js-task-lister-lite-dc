872e24199878cd3615d16c3ac463040a95c314b8
newtypeV2 <gianlibunao@gmail.com>
2019-06-26 10:55:55 -0400

    Refactored code. Moved a function for just getting the input of the user.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
-
-//the form item
 let form = document.getElementById('create-task-form');
-
-//text field where user inputs task item
-let textField = document.getElementById('new-task-description');
-
-//tasks that are listed
-let tasks = document.getElementById('tasks');
-let priority = document.getElementById('priority');
-
 form.addEventListener('submit',submitHandler);
+
+function getTextField(){
+  return document.getElementById('new-task-description').value;
+}
 
 function divExist(priority){
   return document.getElementById(`div-${priority}`) ? true : false
@@ -17,10 +11,10 @@ function divExist(priority){
 
 function createNewLi(string){
   let newTodo = document.createElement('li');
-  newTodo.innerText = string;
   let newButton = document.createElement('button')
-  newButton.innerText = 'X';
   let newEditButton = document.createElement('button')
+  newTodo.innerText = string;
+  newButton.innerText = 'X';
   newEditButton.innerText = 'EDIT';
   newTodo.appendChild(newButton);
   newTodo.appendChild(newEditButton);
@@ -31,8 +25,8 @@ function createNewLi(string){
     const oldText = e.target.parentElement.innerHTML.split("<b")[0]
     let newText = prompt("Please enter new task.")
     while (newText===""){
-      newText = prompt("Please enter new task.","Can't be empty.")
-    }
+        newText = prompt("Please enter new task.","Can't be empty.")
+      }
     (newText === null) ? newTodo.innerText=oldText : newTodo.innerText=newText
     newTodo.appendChild(newButton);
     newTodo.appendChild(newEditButton);
@@ -48,24 +42,27 @@ function divCreator(priority){
     return newDiv;
   }else if(priority==="medium"){ 
     let newDiv = document.createElement("div");
-    newDiv.id = "div-high"
+    newDiv.id = "div-medium"
     newDiv.style.backgroundColor="yellow";
     return newDiv;
   }else{
     let newDiv = document.createElement("div");
-    newDiv.id = "div-high"
+    newDiv.id = "div-low"
     newDiv.style.backgroundColor="green";
     return newDiv;
   }
 }
 
 
-  //add a new event listener for the submission of a new list item
   
-    //handler for submit button for todo list item
+
+
+
 function submitHandler(e) {
-  if (textField.value){
-    let newLi=createNewLi(textField.value);
+  let tasks = document.getElementById('tasks');
+  let priority = document.getElementById('priority');
+  if (getTextField()){
+    let newLi=createNewLi(getTextField());
     if (divExist(priority.value)){
       let priorityDiv = document.getElementById(`div-${priority.value}`);
       priorityDiv.appendChild(newLi);
